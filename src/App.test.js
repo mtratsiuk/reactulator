ae1c2bd1aa12505269c0fb9e6e8e793c1a883f81
Mihail Tratsiuk <mtratsiuk@users.noreply.github.com>
2016-12-21 17:33:12 +0300

    Init :octocat:

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import test from 'ava'
+// import React from 'react'
+// import { shallow } from 'enzyme'
+
+test('Test works', t => {
+  t.truthy(42)
+})
---
+++ webpack.config.dev.js
@@ -0,0 +1,34 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  debug: true,
+  devtool: '#inline-source-map',
+  entry: './src/App.js',
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract(
+          'css?sourceMap!less?sourceMap'
+        )
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new ExtractTextPlugin('bundle.css')
+  ]
+}

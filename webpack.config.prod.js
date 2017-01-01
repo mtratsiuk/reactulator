6104b0ca329f80b9b2b22292df2fec55b5aa5bec
travis <travis>
2017-01-02 00:00:02 +0300

    Fix webpack prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -34,8 +34,7 @@ module.exports = {
     new ExtractTextPlugin('bundle.css'),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': 'production',
-        'BABEL_ENV': 'production'
+        'NODE_ENV': JSON.stringify('production')
       }
     }),
     new webpack.optimize.UglifyJsPlugin({

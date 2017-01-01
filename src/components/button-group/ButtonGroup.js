a1599041b0a8cb7b0791eddfe0bfaea820be5101
Mihail Tratsiuk <mihail.tratsiuk@gmail.com>
2017-01-01 18:50:16 +0300

    Refactor ButtonGroup component

--- src/components/button-group/ButtonGroup.js
+++ src/components/button-group/ButtonGroup.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
 import './ButtonGroup.less'
 
 import Button from '../button/Button'
 
-class ButtonGroup extends Component {
-  render () {
-    const { buttons, dispatch, groupName } = this.props
+export const ButtonGroup = ({ buttons, dispatch, groupName }) =>
+  <section className={`ButtonGroup ButtonGroup_${groupName}`}>
+    {buttons.map(btn =>
+      <Button onClick={() => dispatch(btn.action())}
+        key={btn.text}
+        className={btn.className}>
+        {btn.text}
+      </Button>
+    )}
+  </section>
 
-    return (
-      <section className={`ButtonGroup ButtonGroup_${groupName}`}>
-        {buttons.map(btn =>
-          <Button onClick={() => dispatch(btn.action())}
-            key={btn.text}
-            className={btn.className}>
-            {btn.text}
-          </Button>
-        )}
-      </section>
-    )
-  }
-}
-
-export default connect()(ButtonGroup)
+export default connect(
+  (_, props) => props
+)(ButtonGroup)

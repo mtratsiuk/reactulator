5f692774c32c97b7fe9f85774e1e51fbb057e129
Mihail Tratsiuk <mihail.tratsiuk@gmail.com>
2017-01-01 20:03:24 +0300

    Add prod build

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # reactulator
 
 [![Build Status](https://travis-ci.org/mtratsiuk/reactulator.svg?branch=master)](https://travis-ci.org/mtratsiuk/reactulator)
+[![Coverage Status](https://coveralls.io/repos/github/mtratsiuk/reactulator/badge.svg?branch=master)](https://coveralls.io/github/mtratsiuk/reactulator?branch=master)
---
+++ webpack.config.prod.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract(
+          'css!postcss!less'
+        )
+      }
+    ]
+  },
+  postcss: [
+    require('autoprefixer')({ browsers: ['last 2 versions'] })
+  ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new ExtractTextPlugin('bundle.css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': 'production',
+        'BABEL_ENV': 'production'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true
+    })
+  ]
+}
